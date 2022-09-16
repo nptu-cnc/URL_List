4c80f18f52a0c5817e5c0c39c795b1caf251cfac
karta2341940 <a08487a@gmail.com>
2022-09-16 13:37:05 +0800

    ADD 2 site 新增 E163 ,E162

--- src/render.js
+++ src/render.js
@@ -11,12 +11,17 @@ const list = {
         }
     },
     async created() {
+        //去github抓取要顯示的json檔
         let data = await fetch('https://raw.githubusercontent.com/nptu-cnc/URL_List/main/src/maintainer.json')
+        //將json檔放入上方data中的jsonData
         this.jsonData = await data.json();
         console.log("Created")
+        
         let url = new URL(location.href);
+        //偵測網址列中是否含有參數"o"
         let onlylink = url.searchParams.get("o");
         if (onlylink == 1) {
+            //如果有的話將show設定為false以隱藏部分內容
             this.show = false;
         }
     },
